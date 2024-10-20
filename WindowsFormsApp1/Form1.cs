bdbf5ac2db4ee466ce664f1a6967dfba12b48e8a
Mikk <mik.k00259@gmail.com>
2024-10-20 17:30:40 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Šis metodas išlieka, jei jis naudojamas vėliau.
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Tai yra įvykio tvarkyklė mygtukui. Ją redaguosime, kad atidarytų Form2.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Sukuriame naują Form2 egzempliorių
+            Form2 form2 = new Form2();
+
+            // Rodome Form2 langą
+            form2.Show();
+            this.Hide();
+        }
+    }
+}
